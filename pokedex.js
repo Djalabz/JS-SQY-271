39d6c1fa68e59627a2e91a11eb0f76638c8f43a9
djalabz <djalabz@gmail.com>
2026-02-19 11:39:29 +0100

    :sparkles: Consignes pour Pokedex ajoutées

---
+++ pokedex.js
@@ -0,0 +1,6 @@
+// Faire Pokedex avec la Poke API -> https://pokeapi.co/
+
+// 1 - Faire une requete API avec l'outil de votre choix (axios, ou fetch)
+// 2 - Afficher dès l'initialisation de la page la liste des pokemons (vous pouvez limiter leur nombre)
+// 3 - Pour chaque pokemon -> le nom, la photo et le type
+// 4 - Afficher les résultats de manière propre (grid par exemple) 
