e12b0fc2e19d339f3b6bfb74cf630af09b8a488f
djalabz <djalabz@gmail.com>
2026-02-19 12:29:56 +0100

    :sparkles: Pokedex affiche bien les pokemons

--- pokedex.js
+++ pokedex.js
@@ -1,6 +1,51 @@
-// Faire Pokedex avec la Poke API -> https://pokeapi.co/
+// Faire Pokedex avec la Poke API -> https://pokeapi.co/api/v2/pokemon
 
 // 1 - Faire une requete API avec l'outil de votre choix (axios, ou fetch)
 // 2 - Afficher dès l'initialisation de la page la liste des pokemons (vous pouvez limiter leur nombre)
 // 3 - Pour chaque pokemon -> le nom, la photo et le type
 // 4 - Afficher les résultats de manière propre (grid par exemple) 
+
+// On recu^p le container destiné à recevoir les divs des Pokemons 
+const container = document.querySelector(".container-pokedex")
+
+// L'URL qui permet de recup les pokemons (limité à 20 dans notre cas)
+const pokeUrl = "https://pokeapi.co/api/v2/pokemon"
+
+// La fonction qui vient fetch les pokemons mais aussi créer les éléments HTML et les remplir 
+function fetchPokemons() {
+    fetch(pokeUrl, {
+        method: "GET", 
+        headers : {
+            "Accept" : "application/json"
+        }}
+    )
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+
+        let pokemons = data.results
+
+        pokemons.forEach(pokemon => {
+            let name = pokemon.name
+            let pokemonId = pokemons.indexOf(pokemon) + 1
+            let imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png` 
+
+            // On crée une div, container pour chaque pokemon, un h3 et une image pour le nom et la photo 
+            let div = document.createElement("div")
+            let h3 = document.createElement("h3")
+            let img = document.createElement("img")
+
+            // On donne du contenu à notre h3 et une source pour notre image 
+            h3.textContent = name
+            img.src = imgUrl
+            div.append(h3, img)
+
+            // Enfin on vient insérer le tout dans notre container 
+            container.appendChild(div)
+        })
+
+    })
+    .catch(err => console.log(err))
+}
+
+fetchPokemons()
