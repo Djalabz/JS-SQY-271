fca8ba0b75e6b7c989f614d88b585d30b75508bd
djalabz <djalabz@gmail.com>
2026-03-03 12:43:14 +0100

    :sparkles: Weather API ok exemple avec Yakutsk

--- weather.js
+++ weather.js
@@ -13,20 +13,17 @@
 
 
 const locate = document.querySelector(".locate")
+const container = document.querySelector(".container")
 
 locate.addEventListener("click", () => {
-    getLocation()
+    // getLocation()
+    fetchWeather("62.021964", "129.591533")
 })
 
 
 function getLocation() {
     if (navigator.geolocation) {
-
-        console.log(navigator.geolocation)
-
-        navigator.geolocation.getCurrentPosition(
-
-        (position) => {
+        navigator.geolocation.getCurrentPosition((position) => {
             console.log(position)
         }, 
         error)
@@ -35,9 +32,43 @@ function getLocation() {
     }
 } 
 
-// function fetchWeather(lat, lng) {
-//     console.log(lat, lng)
-// } 
+async function fetchWeather(lat, lng) {
+    // Requete API vers weather API
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=28a46ed081fa271f6e1f3b7415825368&units=metric`
+
+    // Avec await ici je viens fetch la weather API
+    let data = await fetch(apiUrl, { headers: {"Accept" : "applicatopn/json"}})
+
+    // Attention à bien utiliser await également pour la conversion depuis JSON 
+    data = await data.json()
+
+    console.log(data)
+
+    // On recup les infos depuis l'API
+    let city = data.name
+    let temp = data.main.temp
+    let { main, icon, description } = data.weather[0]
+  
+    // On vient ensuite créer les éléments HTML adequats et on leur ajoute 
+    // le contenu texte correspondant
+    let h3 = document.createElement("h3")
+    h3.textContent = city
+
+    let h4 = document.createElement("h4")
+    h4.textContent = temp + "°C"
+
+    let img = document.createElement("img")
+    img.src = `https://openweathermap.org/payload/api/media/file/${icon}.png`
+
+    let h5 = document.createElement("h5")
+    h5.textContent = main
+
+    let p = document.createElement("p")
+    p.textContent = description
+
+    // On vient enfin tout injecter le tout dans notre container
+    container.append(h3, h4, img, h5, p)
+} 
 
 // function success(position) {
     
