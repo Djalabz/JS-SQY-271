cce70892c23a654af931f84320fcd7c78179d0d6
djalabz <djalabz@gmail.com>
2026-03-05 09:45:09 +0100

    :sparkles: Movie app en JS mais avec les classes

--- movies.js
+++ movies.js
@@ -9,7 +9,7 @@
 
 // BONUS : Tenter de le faire avec un système de classes et d'objets 
 
-// Récupération des éléments HTML 
+// Récupération des éléments HTML
 const apiUrl = "http://www.omdbapi.com/?apikey=eed08b06&s="
 const input = document.querySelector(".searchInput")
 const searchBtn = document.querySelector(".searchBtn")
@@ -18,10 +18,55 @@ const resultsZone = document.querySelector(".resultsZone")
 
 let favs = JSON.parse(localStorage.getItem("favorites")) || []
 
+// 3 aspects : Movie, MovieApp, FavManager
+
+// Réfléchirf et coder les 2 prochaines classes : MovieApp et FavManager 
+
+// Dans la classe MovieApp vous aurez les fonctionnalités liées à la recherche de film, 
+// donc par exemple searchMovie() 
+
+// Dans la classe FavManager les fonctionnalités liées aux favoris -> Dans cette classe vous aurez 
+// la propriété favs, les méthodes addToFav() etc
+
+class Movie {
+    constructor(movie) {
+        this.id = movie.imdbId
+        this.title = movie.Title
+        this.poster = movie.Poster
+        this.type = movie.Type
+        this.year = movie.Year 
+    }
+
+    render() {
+        // Créer les éléments HTML : div, titre, image etc 
+        let h3 = document.createElement("h3")
+        let img = document.createElement("img")
+        let h4 = document.createElement("h4")
+        let h5 = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        // Ajouter du contenu à ces éléments 
+        h3.textContent = this.title
+        img.src = this.poster
+        h4.textContent = this.year
+        h5.textContent = this.type
+        btn.textContent = "Ajouter aux favoris"
+
+        btn.addEventListener("click", () => {
+            // Ajout du movie dans les favs
+            favs.push(this)
+            // On enregistre aussi en LS
+            localStorage.setItem("favorites", JSON.stringify(favs))
+        })
+
+        // Insérer ces éléments dans notre div de container 
+        resultsZone.append(h3, img, h4, h5, btn)
+    }
+
+}
 
 // Fonction de recherche de films 
 async function searchMovie(query) {
-
     resultsZone.innerHTML = ""
     input.value = ""
 
@@ -40,40 +85,15 @@ async function searchMovie(query) {
         console.log(movies)
 
         movies.forEach(movie => {
-            
-            displayMovie(movie)
+            // Méthode de type fonctionnelle : :
+            // displayMovie(movie)
 
+            // Méthode de type POO
+            new Movie(movie).render()
         });
     }
 }
 
-function displayMovie(movie) {
-    // Créer les éléments HTML : div, titre, image etc 
-    let h3 = document.createElement("h3")
-    let img = document.createElement("img")
-    let h4 = document.createElement("h4")
-    let h5 = document.createElement("h5")
-    let btn = document.createElement("button")
-
-    // Ajouter du contenu à ces éléments 
-    h3.textContent = movie.title
-    img.src = movie.Poster
-    h4.textContent = movie.Year
-    h5.textContent = movie.Type
-    btn.textContent = "Ajouter aux favoris"
-
-    btn.addEventListener("click", () => {
-        // Ajout du movie dans les favs
-        favs.push(movie)
-
-        // On enregistre aussi en LS
-        localStorage.setItem("favorites", JSON.stringify(favs))
-    })
-
-    // Insérere ces éléments dans notre div de container 
-    resultsZone.append(h3, img, h4, h5, btn)
-
-}
 
 // Ecouteurs d'événements 
 searchBtn.addEventListener("click", () => {
@@ -87,7 +107,35 @@ favBtn.addEventListener("click", () => {
         resultsZone.innerText = "Aucun favori enregistré"
     } else {
         favs.forEach(fav => {
-            displayMovie(fav)
+            new Movie(fav).render()
         })
     }
 }) 
+
+
+
+// function displayMovie(movie) {
+//     // Créer les éléments HTML : div, titre, image etc 
+//     let h3 = document.createElement("h3")
+//     let img = document.createElement("img")
+//     let h4 = document.createElement("h4")
+//     let h5 = document.createElement("h5")
+//     let btn = document.createElement("button")
+
+//     // Ajouter du contenu à ces éléments 
+//     h3.textContent = movie.Title
+//     img.src = movie.Poster
+//     h4.textContent = movie.Year
+//     h5.textContent = movie.Type
+//     btn.textContent = "Ajouter aux favoris"
+
+//     btn.addEventListener("click", () => {
+//         // Ajout du movie dans les favs
+//         favs.push(movie)
+//         // On enregistre aussi en LS
+//         localStorage.setItem("favorites", JSON.stringify(favs))
+//     })
+
+//     // Insérere ces éléments dans notre div de container 
+//     resultsZone.append(h3, img, h4, h5, btn)
+// }
