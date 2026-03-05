75662039d7775c2f91165e888102c8a37454c8af
djalabz <djalabz@gmail.com>
2026-03-05 14:48:26 +0100

    :sparkles: Premiere partie de la citation ok

--- philo.js
+++ philo.js
@@ -10,3 +10,42 @@
 //     Je veux afficher dans un petit container le résumé wikipedia de l'auteur de la citation 
 
 // Vous irez chercher par vous meme le endpoint pour Wikipedia :)
+
+const quote = document.querySelector(".quote")
+const author = document.querySelector(".author")
+const refreshBtn = document.querySelector(".refresh")
+const wikiBtn = document.querySelector(".wiki")
+
+let url = "http://philosophyapi.pythonanywhere.com/api/ideas/"
+let wiki = "http://en.wikipedia.org/api/rest_v1/page/summary/"
+
+let fullUrl = 'https://cors-anywhere.com/' + url
+
+generateQuote(fullUrl)
+
+// On utilise un proxy (corsAnywhere) afin de controuner les restrictions du serveur 
+function generateQuote(fullUrl) {
+
+    let random = Math.floor(Math.random() * 254)
+
+    fetch(fullUrl + random, {
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    })
+    .then(data => data.json())
+    .then(res => {
+        console.log(res)
+    
+        quote.textContent = res.quote
+        author.textContent = res.author
+    
+        
+    })
+    .catch(err => console.log(err)) 
+}
+
+refreshBtn.addEventListener("click", () => {
+    generateQuote(fullUrl)
+}) 
+
